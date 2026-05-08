86803956a9a52d43d6c23e446c5c5fd30d61c646
Marcos Queiroz <marcosamfq123@gmail.com>
2026-05-08 20:42:52 -0300

    fix: reconfig blink for neovim 0.12

--- lua/plugins/code/blink.lua
+++ lua/plugins/code/blink.lua
@@ -1,43 +1,22 @@
 return {
-	"saghen/blink.cmp",
-	version = "v1.*",
-  event = "InsertEnter",
-	dependencies = {
-		"rafamadriz/friendly-snippets",
-	},
-	opts = {
-    cmdline = {
-      enabled = true,
-      keymap = nil,
-      sources = function()
-        local type = vim.fn.getcmdtype()
-        if type == '/' or type == '?' then return { 'buffer' } end
-        if type == ':' then return { 'cmdline' } end
-        return {}
-      end,
-    },
-		keymap = {
-			preset = "none",
-      ["<CR>"] = { "accept", "fallback" },
-      ["<Tab>"] = { "select_next", "fallback" },
-      ["<S-Tab>"] = { "select_prev", "fallback" },
-			["<C-a>"] = { "show_documentation", "hide_documentation" },
-			["<C-y>"] = { "show", "hide" },
-			["<C-e>"] = { "hide" },
-			["<Up>"] = { "select_prev", "fallback" },
-			["<Down>"] = { "select_next", "fallback" },
-			["<C-p>"] = { "select_prev", "fallback" },
-			["<C-n>"] = { "select_next", "fallback" },
-			["<C-b>"] = { "scroll_documentation_up", "fallback" },
-			["<C-f>"] = { "scroll_documentation_down", "fallback" },
-		},
-		completion = {
-			list = {
-				selection = {
-					preselect = false,
-					auto_insert = false,
-				},
-			},
-		},
-	},
+  'saghen/blink.cmp',
+  dependencies = {
+    'saghen/blink.lib',
+    'rafamadriz/friendly-snippets',
+  },
+  build = function()
+    require('blink.cmp').build():wait(60000)
+  end,
+
+  ---@module 'blink.cmp'
+  ---@type blink.cmp.Config
+  opts = {
+    keymap = { preset = 'enter' },
+
+    completion = { documentation = { auto_show = false } },
+
+    sources = { default = { 'lsp', 'path', 'snippets', 'buffer' } },
+
+    fuzzy = { implementation = "rust" }
+  },
 }
