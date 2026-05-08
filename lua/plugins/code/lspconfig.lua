9ef22a47c0ce71e141339c4d741937d315b7c99c
Marcos Queiroz <marcosamfq123@gmail.com>
2026-05-08 18:44:40 -0300

    fix: global vim var warning

--- lua/plugins/code/lspconfig.lua
+++ lua/plugins/code/lspconfig.lua
@@ -1,26 +1,44 @@
 return {
-	"neovim/nvim-lspconfig",
-  event = {"BufReadPre", "BufNewFile"},
-	dependencies = { "saghen/blink.cmp" },
-	config = function()
-		vim.diagnostic.config({
-			virtual_text = {
-				prefix = "●",
-				source = "if_many",
-			},
-			float = {
-				border = "rounded",
-				source = "always",
-			},
-			signs = true,
-			underline = true,
-			update_in_insert = false,
-			severity_sort = true,
-		})
+    "neovim/nvim-lspconfig",
+    event = { "BufReadPre", "BufNewFile" },
+    dependencies = { "saghen/blink.cmp" },
+    config = function()
+        vim.diagnostic.config({
+            virtual_text = {
+                prefix = "●",
+                source = "if_many",
+            },
+            float = {
+                border = "rounded",
+                source = "always",
+            },
+            signs = true,
+            underline = true,
+            update_in_insert = false,
+            severity_sort = true,
+        })
 
-		vim.keymap.set("n", "<leader>ca", vim.lsp.buf.code_action, { desc = "LSP Code Actions" })
-		vim.keymap.set("n", "<leader>cd", vim.diagnostic.open_float, { desc = "Show line diagnostics" })
-		vim.keymap.set("n", "K", vim.lsp.buf.hover, { desc = "Show Documentation" })
-		vim.keymap.set("n", "gd", vim.lsp.buf.definition, { desc = "Go to Definition" })
-	end,
+        vim.keymap.set("n", "<leader>ca", vim.lsp.buf.code_action, { desc = "LSP Code Actions" })
+        vim.keymap.set("n", "<leader>cd", vim.diagnostic.open_float, { desc = "Show line diagnostics" })
+        vim.keymap.set("n", "K", vim.lsp.buf.hover, { desc = "Show Documentation" })
+        vim.keymap.set("n", "gd", vim.lsp.buf.definition, { desc = "Go to Definition" })
+
+        local capabilities = require('blink.cmp').get_lsp_capabilities()
+        local lspconfig = require("lspconfig")
+
+        lspconfig.lua_ls.setup({
+            capabilities = capabilities,
+            settings = {
+                Lua = {
+                    diagnostics = {
+                        globals = { "vim", "Snacks" },
+                    },
+                    workspace = {
+                        library = vim.api.nvim_get_runtime_file("", true),
+                        checkThirdParty = false,
+                    },
+                },
+            },
+        })
+    end,
 }
