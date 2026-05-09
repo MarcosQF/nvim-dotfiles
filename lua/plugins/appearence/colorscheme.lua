643741d544a0ae84197e218ed0a4bed665c2a1bf
Marcos Queiroz <marcosamfq123@gmail.com>
2026-05-09 13:43:40 -0300

    add: kanagawa theme

--- lua/plugins/appearence/colorscheme.lua
+++ lua/plugins/appearence/colorscheme.lua
@@ -1,21 +1,48 @@
-return {
-  {
-    "catppuccin/nvim",
-    name = "catppuccin",
-    lazy = false,
-    priority = 1000,
-    opts = {
-      flavour = "mocha",
-      term_colors = true,
-      background = {
-        light = "latte",
-        dark = "mocha",
-      },
-    },
-    config = function(_, opts)
-      require("catppuccin").setup(opts)
-      vim.cmd.colorscheme "catppuccin"
-    end,
-  },
-}
+-- return {
+--   {
+--     "catppuccin/nvim",
+--     name = "catppuccin",
+--     lazy = false,
+--     priority = 1000,
+--     opts = {
+--       flavour = "mocha",
+--       term_colors = true,
+--       background = {
+--         light = "latte",
+--         dark = "mocha",
+--       },
+--     },
+--     config = function(_, opts)
+--       require("catppuccin").setup(opts)
+--       vim.cmd.colorscheme "catppuccin"
+--     end,
+--   },
+-- }
 
+
+return {
+  "rebelot/kanagawa.nvim",
+  lazy = false,
+  priority = 1000,
+
+  opts= {
+    compile = true,
+    transparent = false,
+    theme = "wave",
+    undercurl = true,
+    colors = {
+      theme = {
+        all = {
+          ui = {
+            bg_gutter = "none"
+          }
+        }
+      }
+    }
+  },
+
+  config = function(_, opts)
+    require("kanagawa").setup(opts)
+    vim.cmd("colorscheme kanagawa")
+  end,
+}
