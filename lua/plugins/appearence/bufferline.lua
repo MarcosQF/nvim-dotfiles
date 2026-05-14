3942c305012704b634a104f9d6e3108bf3b93881
Marcos Queiroz <marcosamfq123@gmail.com>
2026-05-14 18:33:57 -0300

    add: bufferline

--- lua/plugins/appearence/bufferline.lua
+++ lua/plugins/appearence/bufferline.lua
@@ -10,21 +10,19 @@ return {
   opts = {
     options = {
       mode = "buffers",
-      -- O segredo para o visual da imagem está aqui:
-      separator_style = "slant", -- Ou tente "slope" para o efeito inclinado
-      
-      always_show_bufferline = true,
+      separator_style = "thin",
+
+      always_show_bufferline = false,
       show_buffer_close_icons = true,
       show_close_icon = false,
       color_icons = true,
-      
-      -- Ajuste de padding para dar esse ar mais "espaçoso" da foto
+
       left_trunc_marker = '',
       right_trunc_marker = '',
       max_name_length = 18,
       tab_size = 18,
       diagnostics = "nvim_lsp",
-      
+
       offsets = {
         {
           filetype = "NvimTree",
@@ -34,8 +32,6 @@ return {
         }
       },
     },
-    -- Se quiser que as cores batam com o seu tema (Rosé Pine/Gruvbox),
-    -- o bufferline costuma herdar automaticamente, mas o 'slant' destaca as abas.
   },
   config = function(_, opts)
     vim.opt.termguicolors = true
